4c3775ecacb2e8c4a82fdcfd8d2172174f298eca
Jay Wang <iwmq@qq.com>
2025-07-10 21:03:22 +0800

    fix frontend config

--- gin_frontend/vite.config.js
+++ gin_frontend/vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'node:path'
 
+import tailwindcss from '@tailwindcss/vite';
+
 const src_path = path.resolve(__dirname, './src');
 
 // https://vitejs.dev/config/
@@ -22,5 +24,5 @@ export default defineConfig({
   server: {
     origin: 'http://localhost:8080',
   },
-  plugins: [vue()],
+  plugins: [vue(), tailwindcss()],
 })
